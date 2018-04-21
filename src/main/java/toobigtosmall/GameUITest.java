9d65182e8d77af2a4d209b2745e86013a6f21033
Konrad Jałowiecki <dexter2206@gmail.com>
2018-04-21 07:24:04 +0200

    Update IntelliJ files

---
+++ src/main/java/toobigtosmall/GameUITest.java
@@ -0,0 +1,13 @@
+package toobigtosmall;
+
+import java.util.Scanner;
+
+public class GameUITest {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        reader.useDelimiter("\n");
+        String test = reader.next();
+        System.out.println(test);
+    }
+}
